3b2298642659164a7790069b8b26b16d7697aa92
MatheusLBarbosa <barbosa.matheuslopes@gmail.com>
2023-12-13 18:57:19 -0300

    initial commit

---
+++ src/test/java/com/br/apitemplate/ApiTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.apitemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
