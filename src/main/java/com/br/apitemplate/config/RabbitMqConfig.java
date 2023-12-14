4f0af2c4689278d5e5134eebc5f5f95d6da82b63
MatheusLBarbosa <barbosa.matheuslopes@gmail.com>
2023-12-13 21:37:28 -0300

    configure rabbit and application.properties

---
+++ src/main/java/com/br/apitemplate/config/RabbitMqConfig.java
@@ -0,0 +1,44 @@
+package com.br.apitemplate.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+    @Value("${rabbitmq.queue.name}")
+    private String queue;
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+    @Bean
+    Queue queue(){
+        return new Queue(queue);
+    }
+
+    @Bean
+    public TopicExchange exchange(){
+        return new TopicExchange(exchange);
+    }
+    @Bean
+    public Binding binding() {
+        return BindingBuilder
+                .bind(queue())
+                .to(exchange())
+                .with(routingKey);
+    }
+
+    @Bean
+    public MessageConverter messageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+
+}
---
+++ src/main/java/com/br/apitemplate/config/RabbitMqPublisher.java
@@ -0,0 +1,27 @@
+package com.br.apitemplate.config;
+
+import com.br.apitemplate.dto.PedidoDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class RabbitMqPublisher {
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    private RabbitTemplate rabbitTemplate;
+
+    public RabbitMqPublisher(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(PedidoDTO message) {
+        log.info(String.format("Mensagem enviada -> %s", message));
+        rabbitTemplate.convertAndSend(exchange, routingKey, message);
+    }
+}
